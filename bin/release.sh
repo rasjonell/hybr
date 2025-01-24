aa82f56e8f7bd22ee8091420c17b7d8d76931e02
rasjonell <info.gurgen@gmail.com>
2025-01-24 22:57:03 +0400

    Update release script

--- bin/release.sh
+++ bin/release.sh
@@ -1,1 +1,6 @@
-GOOS=linux GOARCH=amd64 go build -o .releases/hybr
+#!/bin/sh
+
+mkdir -p .releases
+
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o .releases/hybr hybr/cmd/cli && upx .releases/hybr
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o .releases/hybr-server hybr/cmd/server && upx .releases/hybr-server
