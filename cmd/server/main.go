dbb856838446e5d04eebb77b801b198d1e4419dd
rasjonell <info.gurgen@gmail.com>
2025-01-29 19:42:38 +0400

    Show Installed services on the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"hybr/internal/services"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from Hybr server")
+		installedServices := services.GetInstalledServices()
+		fmt.Fprintf(w,
+			"Hello from Hybr server\n\nYou have %d Installed Services:\n%s\n",
+			len(installedServices), strings.Join(installedServices, "\n"),
+		)
 	})
 
 	fmt.Println("Server running on :8080")
--- internal/services/utils.go
+++ internal/services/utils.go
@@ -1,5 +1,13 @@
 package services
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
 func GetRegisteredServices() []Service {
 	mu.RLock()
 	defer mu.RUnlock()
@@ -9,4 +17,69 @@ func GetRegisteredServices() []Service {
 		services = append(services, s)
 	}
 	return services
+}
+
+func GetInstalledServices() []string {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	servicesDir := filepath.Join(getWorkingDirectory(), "services")
+
+	var installedServices []string
+	if _, err := os.Stat(servicesDir); os.IsNotExist(err) {
+		fmt.Printf("Services directory not found")
+		return installedServices
+	}
+
+	entries, err := os.ReadDir(servicesDir)
+	if err != nil {
+		fmt.Printf("Unable to read installed services %v\n", err)
+		return installedServices
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			continue
+		}
+
+		installedServices = append(installedServices, entry.Name())
+	}
+
+	return installedServices
+}
+
+func pipeCmdToStdout(cmd *exec.Cmd, label string) error {
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("Error creating stdout pipe: %v", err)
+	}
+
+	stderrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("Error creating stderr pipe: %v", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("Failed to start command for %s: %v", label, err)
+	}
+
+	go func() {
+		scanner := bufio.NewScanner(stdoutPipe)
+		for scanner.Scan() {
+			fmt.Printf("[%s] %s\n", label, scanner.Text())
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stderrPipe)
+		for scanner.Scan() {
+			fmt.Fprintf(os.Stderr, "[%s] %s\n", label, scanner.Text())
+		}
+	}()
+
+	if err := cmd.Wait(); err != nil {
+		return fmt.Errorf("command failed for %s: %v", label, err)
+	}
+
+	return nil
 }
