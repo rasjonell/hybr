0ea9508daea03aa6f53ba49efff7b5267938b423
rasjonell <info.gurgen@gmail.com>
2025-01-29 19:42:22 +0400

    Install Services with the given `InstallCommand`

--- internal/services/generator.go
+++ internal/services/generator.go
@@ -3,6 +3,7 @@ package services
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"text/template"
@@ -37,7 +38,8 @@ func buildTemplateData(vars []*VariableDefinition) map[string]string {
 
 func InstallServices(selected []*SelectedServiceModel) (err error) {
 	for _, service := range selected {
-		servicePath := filepath.Join(getWorkingDirectory(), "services", service.ServiceName, "templates")
+		serviceDir := filepath.Join(getWorkingDirectory(), "services", service.ServiceName)
+		servicePath := filepath.Join(serviceDir, "templates")
 
 		err = filepath.Walk(servicePath, func(path string, info os.FileInfo, err error) error {
 			if err != nil {
@@ -56,7 +58,7 @@ func InstallServices(selected []*SelectedServiceModel) (err error) {
 				return fmt.Errorf("Unable to parse template %s: %w", filename, err)
 			}
 
-			outputPath := filepath.Join(filepath.Dir(path), "..", strings.TrimSuffix(filename, ".templ"))
+			outputPath := filepath.Join(serviceDir, strings.TrimSuffix(filename, ".templ"))
 			out, err := os.Create(outputPath)
 			if err != nil {
 				return err
@@ -75,6 +77,19 @@ func InstallServices(selected []*SelectedServiceModel) (err error) {
 		if err != nil {
 			return err
 		}
+
+		if service.InstallCommand == "" {
+			return fmt.Errorf("Service %s doesn't have an `installCommand`", service.ServiceName)
+		}
+
+		cmd := exec.Command("sh", "-c", service.InstallCommand)
+		cmd.Dir = serviceDir
+
+		if err := pipeCmdToStdout(cmd, service.ServiceName); err != nil {
+			return fmt.Errorf("Unable to install %s Service", service.ServiceName)
+		}
+
+		fmt.Printf("[%s] Is installed and running successfully\n", service.ServiceName)
 	}
 
 	return
