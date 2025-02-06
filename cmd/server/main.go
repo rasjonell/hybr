2ed85b250054fe2e177fa3712827859c86475a9e
rasjonell <info.gurgen@gmail.com>
2025-02-06 17:05:21 +0400

    [Server] Add new dashboard layout

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"hybr/cmd/server/view"
 	"hybr/cmd/server/view/layout"
-	"hybr/cmd/server/view/partials"
 	"log"
 	"net/http"
 
@@ -14,9 +13,7 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	c := layout.Base(view.Index())
-	http.Handle("/", templ.Handler(c))
-	http.Handle("/foo", templ.Handler(partials.ServiceList()))
+	http.Handle("/", templ.Handler(layout.Base(view.Index())))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
