995b644031626a4e4d24c3073e81bf532d590118
rasjonell <info.gurgen@gmail.com>
2025-02-09 19:47:49 +0400

    [Service] Move registry to service

---
+++ internal/services/service.go
@@ -0,0 +1,118 @@
+package services
+
+import (
+	"embed"
+	"encoding/json"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+var (
+	registry = make(map[string]Service)
+	mu       sync.RWMutex
+)
+
+type Service struct {
+	Name           string                `json:"name"`
+	Templates      []string              `json:"templates"`
+	Variables      map[string][]Variable `json:"variables"`
+	SubDomain      bool                  `json:"subDomain"`
+	Description    string                `json:"description"`
+	InstallCommand string                `json:"installCommand"`
+}
+
+type Variable struct {
+	Name        string `json:"name"`
+	Default     string `json:"default"`
+	Description string `json:"description"`
+}
+
+type Template struct {
+	SourcePath string `json:"sourcePath"`
+	TargetName string `json:"targetName"`
+}
+
+type SelectedServiceModel struct {
+	SubDomain      bool
+	ServiceName    string
+	InstallCommand string
+	Variables      map[string][]*VariableDefinition
+}
+
+type VariableDefinition struct {
+	Key   string
+	Value string
+}
+
+func register(s Service) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	registry[s.Name] = s
+}
+
+//go:embed templates/services.json
+var defaultJsonData []byte
+
+//go:embed templates/**/*
+var templatesFS embed.FS
+
+func getServices() []Service {
+	var err error = nil
+	var services []Service
+
+	servicesPath := filepath.Join(getWorkingDirectory(), "services")
+	destPath := filepath.Join(getWorkingDirectory(), "services.json")
+
+	_, err = os.Stat(destPath)
+	if err == nil {
+		data, err := os.ReadFile(destPath)
+		if err != nil {
+			panic("Unable To Read services.json")
+		}
+
+		if err := json.Unmarshal(data, &services); err != nil {
+			panic(err)
+		}
+	}
+
+	if os.IsNotExist(err) {
+		if err := os.WriteFile(destPath, defaultJsonData, 0644); err != nil {
+			panic(err)
+		}
+
+		if err := json.Unmarshal(defaultJsonData, &services); err != nil {
+			panic(err)
+		}
+
+		for _, s := range services {
+			tPath := filepath.Join(servicesPath, s.Name, "templates")
+			if err := os.MkdirAll(tPath, 0755); err != nil {
+				panic(err)
+			}
+
+			for _, templName := range s.Templates {
+				sourceFile, err := templatesFS.Open(filepath.Join("templates", s.Name, templName))
+				if err != nil {
+					panic(err)
+				}
+				defer sourceFile.Close()
+
+				destFile, err := os.Create(filepath.Join(tPath, templName))
+				if err != nil {
+					panic(err)
+				}
+				defer destFile.Close()
+
+				_, err = io.Copy(destFile, sourceFile)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+
+	return services
+}
