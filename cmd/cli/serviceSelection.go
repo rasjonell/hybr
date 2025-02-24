6b687bb6213b644186c42928a5713e8a4979b659
rasjonell <info.gurgen@gmail.com>
2025-02-24 06:32:51 +0400

    [CLI] Add Helper Footer

--- cmd/cli/serviceSelection.go
+++ cmd/cli/serviceSelection.go
@@ -6,41 +6,111 @@ import (
 	"slices"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 )
+
+type selectionKeyMap struct {
+	Up     key.Binding
+	Down   key.Binding
+	Left   key.Binding
+	Right  key.Binding
+	Space  key.Binding
+	Return key.Binding
+	Quit   key.Binding
+	Help   key.Binding
+}
+
+var selectionHelp help.Model = help.New()
+
+func (m *Model) initServiceSelection() {
+	m.cursor = 0
+	m.step = StepServiceSelection
+	selectionHelp.ShowAll = true
+}
+
+func (k selectionKeyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Space, k.Return, k.Help, k.Quit}
+}
+
+func (k selectionKeyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Up, k.Down, k.Left, k.Right},
+		k.ShortHelp(),
+	}
+}
+
+var selectionKeys = selectionKeyMap{
+	Up: key.NewBinding(
+		key.WithKeys("up", "k"),
+		key.WithHelp("↑/k", "move up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down", "j"),
+		key.WithHelp("↓/j", "move down"),
+	),
+	Left: key.NewBinding(
+		key.WithKeys("left", "h"),
+		key.WithHelp("←/h", "move left"),
+	),
+	Right: key.NewBinding(
+		key.WithKeys("right", "l"),
+		key.WithHelp("→/l", "select all"),
+	),
+	Space: key.NewBinding(
+		key.WithKeys(" "),
+		key.WithHelp("␣", "select none"),
+	),
+	Return: key.NewBinding(
+		key.WithKeys("enter"),
+		key.WithHelp("↵", "finish selection"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "ctrl+c"),
+		key.WithHelp("q/ctrl+c", "quit"),
+	),
+	Help: key.NewBinding(
+		key.WithKeys("?"),
+		key.WithHelp("?", "toggle help"),
+	),
+}
 
 func (m *Model) updateServiceSelection(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "ctrl+c":
+		switch {
+		case key.Matches(msg, selectionKeys.Quit):
 			return m, tea.Quit
 
-		case "up", "k":
+		case key.Matches(msg, selectionKeys.Help):
+			selectionHelp.ShowAll = !selectionHelp.ShowAll
+
+		case key.Matches(msg, selectionKeys.Up):
 			if m.cursor == 0 {
 				m.cursor = len(m.services) - 1
 			} else {
 				m.cursor--
 			}
 
-		case "down", "j":
+		case key.Matches(msg, selectionKeys.Down):
 			if m.cursor == len(m.services)-1 {
 				m.cursor = 0
 			} else {
 				m.cursor++
 			}
 
-		case "left", "h":
+		case key.Matches(msg, selectionKeys.Left):
 			m.selectedServiceNames = []string{}
 			m.selected = map[string]services.HybrService{}
 
-		case "right", "l":
+		case key.Matches(msg, selectionKeys.Right):
 			for _, s := range m.services {
 				m.selected[s.GetName()] = s
 				m.selectedServiceNames = append(m.selectedServiceNames, s.GetName())
 			}
 
-		case " ":
+		case key.Matches(msg, selectionKeys.Space):
 			selected := m.services[m.cursor]
 			if _, exists := m.selected[selected.GetName()]; exists {
 				delete(m.selected, selected.GetName())
@@ -52,7 +122,7 @@ func (m *Model) updateServiceSelection(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.selectedServiceNames = append(m.selectedServiceNames, selected.GetName())
 			}
 
-		case "enter":
+		case key.Matches(msg, selectionKeys.Return):
 			if len(m.selectedServiceNames) == 0 {
 				return m, tea.Quit
 			}
@@ -67,6 +137,7 @@ func (m *Model) updateServiceSelection(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m *Model) viewServiceSelection() string {
+	selectionHelp.Width = m.weight
 	var lines []string = []string{
 		"Please Select The Services You Want To Install:\n",
 	}
@@ -82,11 +153,15 @@ func (m *Model) viewServiceSelection() string {
 			selectionIndicator = "  [x]"
 		}
 
-		lines = append(lines, fmt.Sprintf(
-			"%s%s %s",
-			cursorIndicator, selectionIndicator, service.GetDescription()),
+		lines = append(lines,
+			fmt.Sprintf(
+				"%s%s %s",
+				cursorIndicator, selectionIndicator, service.GetDescription(),
+			),
 		)
 	}
+
+	lines = append(lines, "\n", selectionHelp.View(selectionKeys))
 
 	return strings.Join(lines, "\n") + "\n"
 }
--- cmd/cli/variableInput.go
+++ cmd/cli/variableInput.go
@@ -4,29 +4,89 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-func (m *Model) updateVariableInput(msg tea.Msg) (tea.Model, tea.Cmd) {
-	vars := m.getCurrentVariables()
+type variableInputKeyMap struct {
+	CtrlA  key.Binding
+	Return key.Binding
+	Quit   key.Binding
+}
 
+var variableInputHelp help.Model = help.New()
+
+func (k variableInputKeyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Return, k.CtrlA, k.Quit}
+}
+
+func (k variableInputKeyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{k.ShortHelp()}
+}
+
+func getVariableInputKeys(value string) variableInputKeyMap {
+	returnHelp := "save value"
+	if value == "" {
+		returnHelp = "save default value"
+	}
+
+	return variableInputKeyMap{
+		CtrlA: key.NewBinding(
+			key.WithKeys("ctrl+a"),
+			key.WithHelp("ctrl+a", "accept defaults"),
+		),
+		Return: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("↵", returnHelp),
+		),
+		Quit: key.NewBinding(
+			key.WithKeys("esc", "ctrl+c"),
+			key.WithHelp("esc/ctrl+c", "quit"),
+		),
+	}
+}
+
+func (m *Model) setDefaultCurrentVar() {
+	currentVar := m.getCurrentVariables()[m.cursor]
+	if currentVar.Input.Value() == "" {
+		currentVar.Input.SetValue(currentVar.Default)
+	}
+}
+
+func (m *Model) updateVariableInput(msg tea.Msg) (tea.Model, tea.Cmd) {
 	cmds := []tea.Cmd{}
+	vars := m.getCurrentVariables()
 	currentVar := vars[m.cursor]
+
+	currentVarValue := currentVar.Input.Value()
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEnter:
-			if currentVar.Input.Value() == "" {
-				currentVar.Input.SetValue(currentVar.Default)
+		switch {
+		case key.Matches(msg, getVariableInputKeys(currentVarValue).CtrlA):
+			m.setDefaultCurrentVar()
+			for j := m.cursor + 1; j < len(vars); j++ {
+				vars[j].Input.SetValue(vars[j].Default)
 			}
+			m.cursor = 0
+			currentVar.Input.Blur()
+			if m.activeServiceIndex == len(m.selected)-1 {
+				m.step = StepConfirmation
+			} else {
+				m.activeServiceIndex++
+				cmds = append(cmds, m.getCurrentVariables()[m.cursor].Input.Focus())
+			}
+
+		case key.Matches(msg, getVariableInputKeys(currentVarValue).Return):
+			m.setDefaultCurrentVar()
 			if m.cursor == len(vars)-1 {
 				if m.activeServiceIndex == len(m.selected)-1 {
 					m.step = StepConfirmation
 				} else {
-					currentVar.Input.Blur()
-					m.activeServiceIndex++
 					m.cursor = 0
+					m.activeServiceIndex++
+					currentVar.Input.Blur()
 					cmds = append(cmds, m.getCurrentVariables()[m.cursor].Input.Focus())
 				}
 			} else {
@@ -35,7 +95,7 @@ func (m *Model) updateVariableInput(msg tea.Msg) (tea.Model, tea.Cmd) {
 				cmds = append(cmds, vars[m.cursor].Input.Focus())
 			}
 
-		case tea.KeyCtrlC, tea.KeyEsc:
+		case key.Matches(msg, getVariableInputKeys(currentVarValue).Quit):
 			return m, tea.Quit
 		}
 	}
@@ -56,6 +116,9 @@ func (m *Model) viewVariableInput() string {
 			"%d. %s = %s", i+1, v.Name, v.Input.View(),
 		))
 	}
+
+	currentVarValue := m.getCurrentVariables()[m.cursor].Input.Value()
+	lines = append(lines, "\n", variableInputHelp.View(getVariableInputKeys(currentVarValue)))
 
 	return strings.Join(lines, "\n")
 }
