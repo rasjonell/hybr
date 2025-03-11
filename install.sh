ca0b88d56df32974a4ff77e2db83d2d9a82a75d8
rasjonell <info.gurgen@gmail.com>
2025-03-11 19:04:47 +0400

    [Internal] Update release scripts

--- install.sh
+++ install.sh
@@ -34,7 +34,6 @@ print_blue() {
     printf "%s%s%s\n" "$BLUE" "$1" "$NC"
 }
 
-
 print_bold() {
     printf "%s%s%s\n" "$BOLD" "$1" "$NC"
 }
@@ -70,13 +69,13 @@ check_command() {
 get_package_names() {
     case $1 in
         apt-get)
-            echo "golang-go nginx docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin certbot"
+            echo "golang-go docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
             ;;
         pacman)
-            echo "go docker docker-compose nginx certbot"
+            echo "go docker docker-compose"
             ;;
         xbps-install)
-            echo "go docker docker-compose nginx certbot"
+            echo "go docker docker-compose"
             ;;
     esac
 }
@@ -115,7 +114,7 @@ get_install_command() {
 
 check_and_install_dependencies() {
     missing_deps=""
-    deps="go docker docker-compose nginx certbot"
+    deps="go docker docker-compose tailscale"
 
     for dep in $deps; do
         if ! check_command "$dep"; then
@@ -137,6 +136,11 @@ check_and_install_dependencies() {
             [Yy]*)
                 install_cmd=$(get_install_command "$pkg_manager")
                 packages=$(get_package_names "$pkg_manager")
+                tailscale_cmd=""
+                if echo "$missing_deps" | grep -q "tailscale"; then
+                    tailscale_cmd="curl -fsSL https://tailscale.com/install.sh | sudo sh"
+                fi
+
 
                 if [ "$pkg_manager" = "apt-get" ]; then
                     print_blue "Updating package list..."
@@ -144,10 +148,21 @@ check_and_install_dependencies() {
                 fi
 
                 print_blue "Installing packages: $(printf "%s%s%s" "$GREEN" "$packages" "$NC")"
+
                 if ! eval "sudo $install_cmd $packages"; then
                     print_red "Error: Failed to install packages"
                     return 1
                 fi
+
+                if [ -n "$tailscale_cmd" ]; then
+                    print_blue "Installing Tailscale..."
+                    if ! eval "$tailscale_cmd"; then
+                        print_red "Error: Failed to install Tailscale"
+                        return 1
+                    fi
+                fi
+
+
                 print_green "Dependencies installed successfully"
                 return 0
                 ;;
@@ -203,24 +218,41 @@ download_and_run() {
 
     local temp_dir=$(mktemp -d)
     local hybr_cli_url="https://github.com/$repo/releases/download/$version/hybr"
+    local hybr_server_url="https://github.com/$repo/releases/download/$version/hybr-server"
 
     if ! curl -s -L -o "$temp_dir/hybr" "$hybr_cli_url"; then
         print_red "Failed to download CLI binary"
         return 1
     fi
 
+
+    if ! curl -s -L -o "$temp_dir/hybr-console" "$hybr_server_url"; then
+        print_red "Failed to download Web Console binary"
+        return 1
+    fi
+
     chmod +x "$temp_dir/hybr"
+    chmod +x "$temp_dir/hybr-console"
     mkdir -p "$install_dir"
 
-    if ! sudo -p "Please enter your password to install hybr: " mv "$temp_dir/hybr" "$install_dir/"; then
-      print_red "Failed to install hybr binary"
+    echo "TEMP DIR IS:"
+    ls "$temp_dir"
+
+    if ! sudo -p "Please enter your password to install hybr CLI: " mv "$temp_dir/hybr" "$install_dir/"; then
+      print_red "Failed to install hybr CLI binary"
       rm -rf "$temp_dir"
       return 1
     fi
 
-    rm -rf "temp_dir"
+    if ! sudo -p "Please enter your password to install hybr Web Console: " mv "$temp_dir/hybr-console" "$install_dir/"; then
+      print_red "Failed to install hybr Web Console binary"
+      rm -rf "$temp_dir"
+      return 1
+    fi
 
-    if ! sudo "$install_dir/hybr" --forceDefault; then
+    rm -rf "$temp_dir"
+
+    if ! sudo "$install_dir/hybr" init --forceDefaults --no-ssl; then
         print_red "hybr CLI application failed to run"
         return 1
     fi
@@ -235,8 +267,23 @@ if [ $ret_val -ne 0 ]; then
 fi
 
 if check_and_install_dependencies; then
-    sudo usermod -aG docker $USER && newgrp docker
-    if download_and_run "0.0.0"; then
+    # figure out
+    # sudo usermod -aG docker $USER && newgrp docker
+    if download_and_run "v0.0.1"; then
+        read -p "Do you want to start Hybr Web Console? [Y/n] " answer
+
+        if [[ "$answer" == "" || "$answer" == "y" || "$answer" == "Y" ]]; then
+          HYBR_CONSOLE_HOST="/_hybr" /usr/local/bin/hybr-console > /dev/null 2>&1 &
+          
+          read -p "Do you want to expose \`hybr-console: localhost:8080\` on your tailnet? [Y/n] " expose_answer
+          
+          if [[ "$expose_answer" == "" || "$expose_answer" == "y" || "$expose_answer" == "Y" ]]; then
+            sudo tailscale serve --bg --set-path="_hybr" 8080
+          fi
+        fi
+
+        echo "Done!"
+
         exit 0
     else
         print_red "Failed to setup application"
